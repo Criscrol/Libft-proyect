889f2ce4a142147140e394607120c2733460e9c0
CRIS <crsandchess@gmail.com>
2024-09-17 12:11:28 +0200

    c
    añadido ft_strrchr.c

---
+++ ft_strrchr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csanchez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 10:39:42 by csanchez          #+#    #+#             */
+/*   Updated: 2024/09/10 10:41:20 by csanchez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include "libft.h"
+
+char	*ft_strrchr(const char *str, int c)
+{
+	const char	*last;
+
+	last = NULL;
+	while (*str)
+	{
+		if (*str == (char)c)
+		{
+			last = str;
+		}
+		str++;
+	}
+	if (c == '\0')
+	{
+		return ((char *)str);
+	}
+	return ((char *)last);
+}
